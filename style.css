3c2bd8a68e2f34e8e6cdc641ba542041b5e24951
gwkeong <82484673+gwkeong@users.noreply.github.com>
2025-02-19 20:47:55 +0800

    Add files via upload

---
+++ style.css
@@ -0,0 +1,14 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f4f4f4;
+}
+
+.container {
+    margin-top: 50px;
+}
+
+#timer {
+    font-size: 2em;
+    margin-top: 20px;
+}
